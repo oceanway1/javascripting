2c6be6ff1552e34743d0a58438da7e66b33e3846
oceanway1 <53986381+oceanway1@users.noreply.github.com>
2019-10-28 22:27:30 -0700

    Add files via upload

---
+++ for-loop.js
@@ -0,0 +1,10 @@
+var total = 0
+var limit = 10
+for (let i = 0; i < limit; i = i + 1) {
+  total += i
+  if (i == limit - 1) {
+    console.log(total)
+  }
+}
+
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,7 @@
+const pets = ['cat', 'dog', 'rat']  
+for (let i =0; i < 1; i=i+1)  {
+pets[0] = pets[0] + 's'
+pets[1] = pets[1] + 's'
+pets[2] = pets[2] + 's'
+  console.log (pets)
+}
